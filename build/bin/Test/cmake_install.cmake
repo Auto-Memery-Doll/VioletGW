cac9a3875fced26105df1e83d607aff889edd358
violet <3101298914@qq.com>
2026-07-24 18:22:02 +0800

    remove other middle file

--- build/bin/Test/cmake_install.cmake
+++ build/bin/Test/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/xiugou/flow_gateway/test
+# Install script for directory: /home/violet/flow_gateway/test
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -44,10 +44,8 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/xiugou/flow_gateway/build/bin/Test/lwip/cmake_install.cmake")
-  include("/home/xiugou/flow_gateway/build/bin/Test/dpdk/cmake_install.cmake")
-  include("/home/xiugou/flow_gateway/build/bin/Test/base/cmake_install.cmake")
-  include("/home/xiugou/flow_gateway/build/bin/Test/cache/cmake_install.cmake")
+  include("/home/violet/flow_gateway/build/bin/Test/base/cmake_install.cmake")
+  include("/home/violet/flow_gateway/build/bin/Test/dpdk/cmake_install.cmake")
 
 endif()
 
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/xiugou/flow_gateway
+# Install script for directory: /home/violet/flow_gateway
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -44,8 +44,9 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/xiugou/flow_gateway/build/bin/Src/cmake_install.cmake")
-  include("/home/xiugou/flow_gateway/build/bin/Test/cmake_install.cmake")
+  include("/home/violet/flow_gateway/build/_deps/spdlog-build/cmake_install.cmake")
+  include("/home/violet/flow_gateway/build/bin/Src/cmake_install.cmake")
+  include("/home/violet/flow_gateway/build/bin/Test/cmake_install.cmake")
 
 endif()
 
@@ -57,5 +58,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/xiugou/flow_gateway/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/violet/flow_gateway/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
